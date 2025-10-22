4353d856559d8505361f4944a5000b82d4898b99
david <david.villca.pacheco@gmail.com>
2025-10-21 23:13:41 -0400

    init project

---
+++ src/main/java/com/blumbit/compras_ventas/controller/StatusController.java
@@ -0,0 +1,13 @@
+package com.blumbit.compras_ventas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public String statusApp() {
+        return "Application Running";
+    }
+}
---
+++ src/test/java/com/blumbit/compras_ventas/ComprasVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blumbit.compras_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
